65bd9a7f74f2839a605c3f254c86bbcf03e297f5
ZacharyMohler <95890882+ZacharyMohler@users.noreply.github.com>
2023-08-11 15:10:04 -0700

    Update server.js

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/zmmg'));
+app.use(express.static(__dirname + '/dist/zmmg-df99072f6704'));
 
 app.get('/*', function (req, res)
 {
 
-  res.sendFile(path.join(__dirname + '/dist/zmmg/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/zmmg-df99072f6704/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
